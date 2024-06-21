2081aa9538aa2bb41db1382c5158c34b2660fcc0
QunLee273 <quan2732k3@gmail.com>
2024-06-21 19:04:09 +0700

    xong giao diện giảng viên

--- Quân/main_GV.py
+++ Quân/main_GV.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import sqlite3
-from tkinter import ttk
+from tkinter import messagebox, ttk
 
 main_GV = Tk()
 main_GV.title("Giảng viên")
@@ -15,78 +15,119 @@ def manHinh(event):
 
 
 def ht_infor():
-    # Tạo cửa sổ mới
-    win_infor = Toplevel(main_GV)
-    win_infor.title("Thông tin Giảng viên")
+    try:
+        # Xóa dữ liệu cũ trong Treeview (nếu có)
+        for i in bang.get_children():
+            bang.delete(i)
 
-    # Tạo và cấu hình Treeview
-    tree = ttk.Treeview(win_infor, columns=["mgv", "hoten", "nsinh", "gioiT", "diaC", "mail", "sdt"], show='headings')
-    tree.heading("mgv", text="Mã GV")
-    tree.heading("hoten", text="Họ tên")
-    tree.heading("nsinh", text="Ngày sinh")
-    tree.heading("gioiT", text="Giới tính")
-    tree.heading("diaC", text="Địa chỉ")
-    tree.heading("mail", text="Email")
-    tree.heading("sdt", text="Số điện thoại")
-    tree.pack()
 
-    # Lấy dữ liệu từ database
-    cur.execute("SELECT * FROM GiangVien WHERE MaGV = ?", (ten_ht,))
-    rows = cur.fetchall()
+        # Lấy dữ liệu từ database
+        cur.execute("SELECT * FROM GiangVien WHERE MaGV = ?", (ten_ht,))
+        tTin = cur.fetchone()
 
-    # Hiển thị dữ liệu trong Treeview
-    for row in rows:
-        tree.insert("", "end", values=row)
+        # Cấu hình Treeview
+        bang.config(columns=["c1", "c2", "c3", "c4"], show="headings")
+        bang.place(x=400, y=150, width=600, height=400)
+
+        # Đặt kích thước ô
+        bang.column("#0", width=0)
+        bang.column("c1", width=100)
+        bang.column("c2", width=200)
+        bang.column("c3", width=100)
+        bang.column("c4", width=200)
+
+        data = [
+            ("MaGV:", tTin[0], "Địa chỉ:", tTin[4]),
+            ("", "", "", ""),
+            ("", "", "", ""),
+            ("Họ tên:", tTin[1], "Email:", tTin[5]),
+            ("", "", "", ""),
+            ("", "", "", ""),
+            ("Ngày sinh:", tTin[2], "SĐT:", tTin[6]),
+            ("", "", "", ""),
+            ("", "", "", ""),
+            ("Giới tính:", tTin[3], "", "")
+        ]
+
+        # Hiển thị dữ liệu trong Treeview
+        for i in data:
+            bang.insert("", "end", values=i)
+
+    except sqlite3.Error as e:
+        messagebox.showerror("Lỗi", f"Lỗi: {e}")
 
 def ht_phanC():
-    # Tạo cửa sổ mới
-    win_phanC = Toplevel(main_GV)
-    win_phanC.title("Phân công giảng dạy")
+    try:
+        # Xóa dữ liệu cũ trong Treeview (nếu có)
+        for i in bang.get_children():
+            bang.delete(i)
 
-    # Tạo và cấu hình Treeview
-    tree = ttk.Treeview(win_phanC, columns=["mapc", "hp", "hocKy", "nam", "lop"], show='headings')
-    tree.heading("mapc", text="MaPC")
-    tree.heading("hp", text="Học phần")
-    tree.heading("hocKy", text="Học kỳ")
-    tree.heading("nam", text="Năm học")
-    tree.heading("lop", text="Lớp")
-    tree.pack()
+        # Cấu hình Treeview
+        bang.config(columns=["mapc", "hp", "hocKy", "nam", "lop"], show='headings')
+        bang.heading("mapc", text="MaPC")
+        bang.heading("hp", text="Học phần")
+        bang.heading("hocKy", text="Học kỳ")
+        bang.heading("nam", text="Năm học")
+        bang.heading("lop", text="Lớp")
+        bang.place(x=400, y=150, width=600, height=400)
 
-    # Lấy dữ liệu từ database
-    cur.execute(
-        """SELECT PhanCong.MaPC, HocPhan.TenHP, PhanCong.HocKy, PhanCong.NamHoc, Lop.TenLop 
-            FROM PhanCong, HocPhan, Lop
-            WHERE PhanCong.MaHP = HocPhan.MaHP AND PhanCong.MaLop = Lop.MaLop AND PhanCOng.MaGV = ?
-            """, (ten_ht, ))
-    rows = cur.fetchall()
+        # Đặt kích thước ô
+        bang.column("mapc", width=30)
+        bang.column("hp", width=200)
+        bang.column("hocKy", width=50)
+        bang.column("nam", width=100)
+        bang.column("lop", width=100)
 
-    # Hiển thị dữ liệu trong Treeview
-    for row in rows:
-        tree.insert("", "end", values=row)
+        # Lấy dữ liệu từ database
+        cur.execute(
+            """SELECT PhanCong.MaPC, HocPhan.TenHP, PhanCong.HocKy, PhanCong.NamHoc, Lop.TenLop 
+                FROM PhanCong, HocPhan, Lop
+                WHERE PhanCong.MaHP = HocPhan.MaHP AND PhanCong.MaLop = Lop.MaLop AND PhanCOng.MaGV = ?
+                """, (ten_ht, ))
+        rows = cur.fetchall()
+
+        # Hiển thị dữ liệu trong Treeview
+        for row in rows:
+            bang.insert("", "end", values=row)
+
+    except sqlite3.Error as e:
+        messagebox.showerror("Lỗi", f"Lỗi: {e}")
 
 def ht_danhGia():
-    # Tạo cửa sổ mới
-    win_danhGia = Toplevel(main_GV)
-    win_danhGia.title("Đánh giá giảng viên")
+    try:
+        # Xóa dữ liệu cũ trong Treeview (nếu có)
+        for i in bang.get_children():
+            bang.delete(i)
 
-    # Tạo và cấu hình Treeview
-    tree = ttk.Treeview(win_danhGia, columns=["madg", "noidung", "diem", "ngay"], show='headings')
-    tree.heading("madg", text="MaDG")
-    tree.heading("noidung", text="Nội dung")
-    tree.heading("diem", text="Điểm số")
-    tree.heading("ngay", text="Ngày đánh giá")
-    tree.pack()
+        # Cấu hình Treeview
+        bang.config(columns=["madg", "noidung", "diem", "ngay"], show='headings')
+        bang.heading("madg", text="MaDG")
+        bang.heading("noidung", text="Nội dung")
+        bang.heading("diem", text="Điểm số")
+        bang.heading("ngay", text="Ngày đánh giá")
+        bang.place(x=400, y=150, width=600, height=400)
 
-    # Lấy dữ liệu từ database
-    cur.execute(
-        """SELECT MaDG, NoiDung, DiemSo, NgayDanhGia 
-            FROM DanhGia 
-            WHERE MaGV = ? """, (ten_ht, ))
-    rows = cur.fetchall()
+        # Đặt kích thước ô
+        bang.column("madg", width=30)
+        bang.column("noidung", width=200)
+        bang.column("diem", width=50)
+        bang.column("ngay", width=100)
 
-    # Hiển thị dữ liệu trong Treeview
-    for row in rows:
-        tree.insert("", "end", values=row)
+        # Lấy dữ liệu từ database
+        cur.execute(
+            """SELECT MaDG, NoiDung, DiemSo, NgayDanhGia 
+                FROM DanhGia 
+                WHERE MaGV = ? """, (ten_ht, ))
+        rows = cur.fetchall()
+
+        # Hiển thị dữ liệu trong Treeview
+        for row in rows:
+            bang.insert("", "end", values=row)
+
+    except sqlite3.Error as e:
+        messagebox.showerror("Lỗi", f"Lỗi: {e}")
+
+
 
 
 lb_GV = Label(main_GV, font=("Arial", 18, 'bold'), bd=2, relief=SOLID)
@@ -101,8 +142,13 @@ btn_pC.place(x=50, y=200, width=200, height=40)
 btn_dg = Button(main_GV, text="Xem đánh giá", width=15, font=("Arial", 12, 'bold'), command=ht_danhGia)
 btn_dg.place(x=50, y=250, width=200, height=40)
 
+lb_tt = Label(main_GV, font=("Arial", 12), justify="left")
+
 btn_dangX = Button(main_GV, text="Đăng xuất", width=15, font=("Arial", 12, 'bold'), command=exit)
 btn_dangX.place(x=50, y=500, width=200, height=40)
+
+# Tạo bảng
+bang = ttk.Treeview(main_GV)
 
 # Hiển thị tên
 with open('HienThi.txt', 'r') as f:
